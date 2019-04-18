58034be3a37033a63ee9ca65ed41a1dd19059fa4
Richartrex <learic7@gmail.com>
2019-04-18 21:10:37 +0200

    Create button sexe and genre

---
+++ src/app/button-sexe/button-sexe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-sexe',
+  templateUrl: './button-sexe.component.html',
+  styleUrls: ['./button-sexe.component.css']
+})
+export class ButtonSexeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+public createSexe() {}
+}
