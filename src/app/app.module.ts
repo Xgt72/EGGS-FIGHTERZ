fda37dacc1611084a84f5f9864d1582ca480bc27
Patrice Robin <rob1.pat81@gmail.com>
2019-04-18 22:23:27 +0200

    creation des cartes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarteComponent } from './carte/carte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarteComponent
   ],
   imports: [
     BrowserModule,
